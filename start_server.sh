6568f1bd9f6dfa87e91d2b34ef60031a67804050
tengxunyundoge <asjhc45624@qq.com>
2024-10-04 01:38:04 +0800

    tickrate-enabler

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-./srcds_run -game left4dead2 -secure +hostport 27015 -condebug +map c1m1_hotel +exec server.cfg -nomaster
+./srcds_run -game left4dead2 -secure +hostport 27016 -condebug +map c2m1_highway +exec server.cfg -tickrate 100
